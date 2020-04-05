50493634f8f4912858433c601a4119ab0cecc3e7
Chuck McManis <cmcmanis@mcmanis.com>
2020-04-04 19:56:17 -0700

    UART code
    This commit adds some functions to aid with using the UART that
    is presented on the Nucleo's USB interface in your programs.

--- nucleo-f446re/common.h
+++ nucleo-f446re/common.h
@@ -1,16 +1,59 @@
 /*
- * This is an include file that describes the functions available
- * in the common code. If you use them be sure to include the .o
- * files from the common code in your example.
+ * Common API definitions include file.
+ *
+ * BSD 2-Clause License
+ * 
+ * Copyright (C) 2013-2020 Chuck McManis <cmcmanis@mcmanis.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <stdint.h>
 #pragma once
+
+/*
+ * Clock setup and use API
+ */
 void nucleo_clock_setup(void);
 void msleep(uint32_t delay);
 uint32_t mtime(void);
 unsigned char *time_string(uint32_t t);
 
+/*
+ * LED setup and use API
+ */
 void led_setup(void);
 void led_on(void);
 void led_off(void);
 int	led_toggle(void);
 
+/*
+ * UART setup and use API
+ */
+void uart_putc(char c);
+char uart_getc(int wait);
+void uart_puts(char *s);
+int uart_gets(char *s, int len);
+void uart_setup(int baud);
+void uart_baud(int baud_rate);
